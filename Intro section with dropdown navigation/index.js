cb7796c5fdeb47a653f3ddc12413f3718a21f2f8
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2023-06-10 23:04:45 +0500

    final navbar

--- Intro section with dropdown navigation/index.js
+++ Intro section with dropdown navigation/index.js
@@ -2,6 +2,10 @@ const showNav = document.querySelector(".showNav");
 const show = document.querySelector(".show");
 const showNav1 = document.querySelector(".showNav1");
 const show1 = document.querySelector(".show1")
+const navshw = document.querySelector(".navshw")
+const hamClick = document.querySelector(".hamClick")
+const cross = document.getElementById('cross');
+
 
 
 show.addEventListener('click', function(e){
@@ -9,6 +13,18 @@ show.addEventListener('click', function(e){
     showNav.classList.toggle('hidden')
     
 })
+hamClick.addEventListener('click', function(e){
+    e.preventDefault()
+    navshw.classList.remove('hidden')
+    
+})
+
+cross.addEventListener('click', function(e){
+    e.preventDefault()
+    navshw.classList.add('hidden')
+    
+})
+
 
 show1.addEventListener('click', function(e){
     e.preventDefault()
